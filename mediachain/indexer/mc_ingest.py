d32270ad60614ab3c603f545d3740faac8ee488f
autoencoder <tim@mergeai.com>
2016-06-08 17:48:04 -0400

    Keywords typo

--- mediachain/indexer/mc_ingest.py
+++ mediachain/indexer/mc_ingest.py
@@ -364,7 +364,7 @@ def ingest_bulk_blockchain(last_block_ref = None,
                         if type(kk) == tuple:
                             rh[kk[1]] = meta[kk[0]]
                         elif kk == u'keywords':
-                            rh[kk[1]] = meta[' '.join(kk[0])]
+                            rh[kk] = meta[' '.join(kk)]
                         else:
                             rh[kk] = meta[kk]
                     
