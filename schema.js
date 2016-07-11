a89e577cff43ae607b7e5eeda600d0a39a5f0d8e
autoencoder <tim@mergeai.com>
2016-07-11 18:08:31 -0400

    Schema Discussion.

---
+++ schema.js
@@ -0,0 +1,119 @@
+{
+    "_id": "getty_WA0682-001",
+    "source_name": "getty", 
+    "date_created_original": "1979-10-17", 
+    "date_created_at_source": "2005-08-31T00:00:00-07:00", 
+    "mediachain_record": null,
+    "title": [
+        "Male boxer practising using punch bag (blurred motion, toned B&W)"
+    ], 
+    "artist_names": [
+        "Mike Powell"
+    ], 
+    "attribution_list": [
+        {
+            "name": "Mike Powell",
+            "role": "artist", 
+            "details": null
+        }
+    ],
+    "provider_list": [
+        {
+            "name": "dpla"
+        }, 
+        {
+            "name": "Digital Library of Georgia"
+        }
+    ],
+    "sightings_list": [
+        {
+            "url_page": "http://digitalcollections.library.gsu.edu/cdm/ref/collection/ajc/id/8135", 
+            "url_image": "http://dlg.galileo.usg.edu/gsu/ajc/do-th:8135"
+        }
+    ], 
+    "licenses_list": [
+        {
+            "name_long": ["Attribution-NoDerivs License"],
+            "attribute_to": [],
+            "details": []
+        }
+    ], 
+    "keywords": [
+        "Speed", 
+        "Skill", 
+        "Sport", 
+        "Horizontal", 
+        "Blurred Motion", 
+        "Indoors", 
+        "20-24 Years", 
+        "Preparation", 
+        "Headshot"
+    ], 
+    "orientation": "Horizontal", 
+    "sizes": [
+        {
+            "bytes": 1319, 
+            "height": 44, 
+            "uri_external": null, 
+            "width": 66, 
+            "content_type": "image/jpeg", 
+            "dpi": null
+        }, 
+        {
+            "bytes": 4715787, 
+            "height": 3364, 
+            "uri_external": null, 
+            "width": 5065, 
+            "content_type": "image/jpeg", 
+            "dpi": null
+        }
+    ], 
+    "camera_exif": {
+        "-Brightness": "665/100", 
+        "-Date and Time": "2008:10:05 01:38:02", 
+        "-Focal Plane Resolution Unit": "3", 
+        "-Software": "Adobe Photoshop CS3 Windows", 
+        "-X-Resolution": "720000/10000", 
+        "-Resolution Unit": "2", 
+        "-Compression": "6", 
+        "-Pixel X-Dimension": "2285", 
+        "-Y-Resolution": "720000/10000", 
+        "-Date and Time (Digitized)": "2007:04:16 15:17:24", 
+        "-Model": "FinePix S5600", 
+        "-Focal Plane X-Resolution": "2741/1", 
+        "-Orientation": "1", 
+        "-Aperture": "3356144/1000000", 
+        "-Maximum Lens Aperture": "340/100", 
+        "-Flash": "16", 
+        "-Sensing Method": "2", 
+        "-Focal Length": "820/100", 
+        "-Exposure": "1/320", 
+        "-Focal Plane Y-Resolution": "2741/1", 
+        "-Metering Mode": "5", 
+        "-Has XMP block": "1", 
+        "-Make": "FUJIFILM", 
+        "-Exposure Bias": "0/100", 
+        "-Date and Time (Original)": "2007:04:16 15:17:24", 
+        "-ISO Speed": "100", 
+        "-Pixel Y-Dimension": "1714", 
+        "-Exposure Program": "2", 
+        "-Color Space": "1", 
+        "-Shutter Speed": "8321928/1000000"
+    }, 
+    "transient_info": {
+        "score_hotnessviews": null, 
+        "likes": null
+    }, 
+    "derived_qualities": {
+        "medium": "photography", 
+        "predicted_tags": null, 
+        "has_people": null, 
+        "colors": null, 
+        "general_type": null, 
+        "time_period": null
+    }, 
+    "location": {
+        "lat_lon": null, 
+        "place_name": []
+    }
+}
