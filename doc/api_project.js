be3908abb23b2853749000dfc57b4c26f32189cd
denisnazarov <zvezda@gmail.com>
2016-07-27 19:17:55 -0400

    Add API Docs.

---
+++ doc/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-07-27T23:17:34.515Z",    "url": "http://apidocjs.com",    "version": "0.16.1"  }});
